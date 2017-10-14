24976ff2882ce1b5e49baa98c88a5dad9c4a4d04
Mhirra <mhirralunayung@gmail.com>
2017-10-14 13:19:19 -0500

    Add files via upload

--- assets/js/weatherWidget.js
+++ assets/js/weatherWidget.js
@@ -1,6 +1,1 @@
-<div class="weather">
-			<div class ="card-header" id ="currweather-city-1"></div>
-			<div class ="forecast">
-				<!--<div class = "card" id ="forecast-1-city-1"></div>-->
-			</div>
-		</div>
+weatherWidget.js
