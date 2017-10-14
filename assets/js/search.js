4ca9cff9b56e774e6035dec2cb109339f5a67ee9
dvastro <stroshid@oregonstate.edu>
2017-10-14 13:55:07 -0500

    adding search file

--- assets/js/search.js
+++ assets/js/search.js
@@ -9,3 +9,30 @@
         console.log(temp)
         });
   }
+
+
+// FIREBASE CODE COMMENTED OUT
+
+// function intialize(callback){
+
+//     autocomplete = new google.maps.places.Autocomplete(
+// 	    (document.getElementById('autocomplete')),
+// 	    { types: ["(cities)"] });
+// 	    google.maps.event.addListener(autocomplete, 'place_changed', function(callback) {
+// 		    temp=autocomplete.getPlace();
+// 	    	var newPlace = {
+// 	    		locality: temp.locality,
+// 	    		region: temp.region,
+// 	    		country: temp.country-name,
+// 	    		lat: temp.lat,
+// 	    		long: temp.lng, 
+
+// 	    	};
+// 	    	callback(newPlace);
+// 		    console.log(temp);
+// 		    console.log(callback);
+
+//     });
+
+// }
+
